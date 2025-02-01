d69466828937b9c7b0d60a98f223e72eb5571586
임주완 <imjuwan@imjuwan-ui-MacBookAir.local>
2025-02-01 16:41:55 +0900

    update: added dataset scheduler

---
+++ dags/my_consumer.py
@@ -0,0 +1,21 @@
+# my_consumer.py
+
+from airflow import DAG
+from airflow.datasets import Dataset
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+local_file = Dataset('/tmp/sample.txt')
+
+with DAG(
+    dag_id="my_consumer",
+    schedule=[local_file],
+    start_date=datetime(2025,1,1),
+    catchup=False
+) as dag:
+    
+    task_consumer = BashOperator(
+        task_id = 'consumer1',
+        bash_command='cat /tmp/sample.txt'
+    )
+
